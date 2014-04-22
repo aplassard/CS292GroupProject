7edc7524dfbbabe15bcabe10cf6075776181c09d
chambm <matt.chambers42@gmail.com>
2014-04-21 20:20:04 -0500

    - added an orphaned generate_users page for testing purposes - changed project name and added description text - made the jumbotron always visible on the main page - added escaping of search regexes so searches like "C++" work

--- server/publications.js
+++ server/publications.js
@@ -6,18 +6,23 @@ Meteor.publish('allUserData', function(ids) {
   }
 });
 
+// short 'n sweet: http://stackoverflow.com/questions/3446170
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
 Meteor.publish('listUsers', function(searchTerm, searchWhat) {
   if (this.userId) {
     var searchObject = {}
     if (searchWhat === "name") {
       if (searchTerm) {
-        var regex = new RegExp("\w*" + searchTerm + "\w*", 'i');
+        var regex = new RegExp("\w*" + escapeRegExp(searchTerm) + "\w*", 'i');
         searchObject = {$or: [{username: regex}, {'profile.name': regex}]};
       }
     } else {
       if (searchTerm) {
         if (searchWhat === 'skills') {
-          var regex = new RegExp(searchTerm, 'i');
+          var regex = new RegExp(escapeRegExp(searchTerm), 'i');
           searchObject = {'profile.skills': regex};
         } else {
           searchObject = {'profile.interests': regex};
